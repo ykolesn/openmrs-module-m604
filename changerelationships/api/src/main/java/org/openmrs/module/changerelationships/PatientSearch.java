76d9ed1e0ad9cb8bde5b65a1a7b54036f167477d
yuri <ykolesn@gmail.com>
2013-03-13 00:14:29 -0400

    patientsearch commit

---
+++ changerelationships/api/src/main/java/org/openmrs/module/changerelationships/PatientSearch.java
@@ -0,0 +1,19 @@
+package org.openmrs.module.changerelationships;
+
+public class PatientSearch {
+	private String name;
+	private String relationshipType;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRelationshipType() {
+		return relationshipType;
+	}
+	public void setRelationshipType(String relationshipType) {
+		this.relationshipType = relationshipType;
+	}	
+
+}
